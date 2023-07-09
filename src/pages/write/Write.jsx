699e0da2180d81597d2b561a1bdc5eda18e209cd
Mohamed-Abbes <mohamedabbes001@gmail.com>
2023-07-09 03:09:02 +0200

    eighth commit

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -25,7 +25,7 @@ export default function Write() {
       data.append("file", file);
       newPost.photo = filename;
       try {
-        await axios.post("/api/upload", data);
+        await axios.post("/https://blog-upp.onrender.comapi/upload", data);
       } catch (err) {}
     }
     try {
