3d066196a105419c64aaf625c798884b69c334da
Mohamed-Abbes <mohamedabbes001@gmail.com>
2023-07-09 18:33:41 +0200

    25

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,7 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://api.cloudinary.com/v1_1/dk7f4rass/image/upload/";
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
