f0dc036e140634c85a42c27238b99e883e715be1
Mohamed-Abbes <mohamedabbes001@gmail.com>
2023-07-09 16:44:47 +0200

    16 commit

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -25,7 +25,7 @@ export default function Write() {
       data.append("file", file);
       newPost.photo = filename;
       try {
-        await axios.post("/api/upload", data);
+        await axios.post("https://api.cloudinary.com/v1_1/dk7f4rass/image/upload", data);
       } catch (err) {}
     }
     try {
